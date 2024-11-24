8e3403f200223c62122e85c479eb43e48ce59b4b
Moonflower-labs <134706556+Moonflower-labs@users.noreply.github.com>
2024-11-24 18:33:56 +0000

    fix admin nav

--- app/routes/admin/layout.tsx
+++ app/routes/admin/layout.tsx
@@ -1,30 +1,41 @@
 import { AiFillProduct } from "react-icons/ai";
 import { BiCategoryAlt, BiSolidVideos } from "react-icons/bi";
-import { FaQuestion } from "react-icons/fa";
+import { FaHome, FaQuestion } from "react-icons/fa";
 import { GiQuillInk } from "react-icons/gi";
 import { HiMiniShoppingBag } from "react-icons/hi2";
 import { PiUsersThreeFill } from "react-icons/pi";
+import { RiWebhookFill } from "react-icons/ri";
 import { NavLink, Outlet } from "react-router";
-import tarotIcon from "~/icons/tarot.svg"
 import type { Route } from "./+types/layout";
 import { requireUserId } from "~/utils/session.server";
 
 export async function loader({ request }: Route.LoaderArgs) {
-  return requireUserId(request)
+  return await requireUserId(request);
 }
 
 export default function AdminLayout() {
   return (
-    <div className="flex flex-col md:flex-row">
-      <div className="hidden md:block w-64 shrink-0 h-screen" />
+    <div className="flex flex-col md:flex-row min-h-screen">
+      <div className="hidden md:block w-52 shrink-0" />
       <div
         role="tablist"
-        className="bg-base-100 flex flex-row md:flex-col flex-wrap gap-1 pt-1 justify-evenly md:justify-start w-full md:w-64 sticky top-[72px] md:fixed md:left-0 z-50 rounded-lg">
+        className="bg-base-200 flex flex-wrap md:flex-nowrap flex-row md:flex-col gap-1 pt-1 justify-center md:justify-start w-full overflow-x-auto md:w-52 sticky top-[72px] md:fixed md:left-0 z-50 h-auto md:h-[calc(100vh-72px)] rounded-lg">
+
+        <NavLink
+          to={"admin"}
+          role="tab"
+          className={({ isActive }) =>
+            `flex flex-col justify-start items-center md:flex-row gap-2 border rounded-md p-2 ${isActive && "bg-primary text-primary-content"}`
+          }
+          end
+          viewTransition>
+          <FaHome size={24} />
+        </NavLink>
         <NavLink
           to={"/admin/post"}
           role="tab"
           className={({ isActive }) =>
-            `flex flex-col justify-center items-center md:flex-row gap-2 border rounded-md p-2 ${isActive && "bg-primary text-primary-content"}`
+            `flex flex-col justify-start items-center md:flex-row gap-2 border rounded-md p-2 ${isActive && "bg-primary text-primary-content"}`
           }
           viewTransition>
           <GiQuillInk size={20} />
@@ -34,31 +45,17 @@ export default function AdminLayout() {
           to={"/admin/videos"}
           role="tab"
           className={({ isActive }) =>
-            `flex flex-col justify-center items-center md:flex-row gap-2 border rounded-md p-2 ${isActive && "bg-primary text-primary-content"}`
+            `flex flex-col justify-start items-center md:flex-row gap-2 border rounded-md p-2 ${isActive && "bg-primary text-primary-content"}`
           }
           viewTransition>
           <BiSolidVideos size={20} />
           <span className="text-xs md:text-lg">Videos</span>
         </NavLink>
         <NavLink
-          to={"admin"}
-          role="tab"
-          className={({ isActive }) =>
-            `flex flex-col justify-center items-center md:flex-row gap-2 border rounded-md p-2 ${isActive && "bg-primary text-primary-content"}`
-          }
-          viewTransition>
-          <div className="avatar">
-            <div className="w-6 rounded">
-              <img src={tarotIcon} />
-            </div>
-          </div>
-          <span className="text-xs md:text-lg">Tarot</span>
-        </NavLink>
-        <NavLink
           to={"/admin/categories"}
           role="tab"
           className={({ isActive }) =>
-            `flex flex-col justify-center items-center md:flex-row gap-2 border rounded-md p-2 ${isActive && "bg-primary text-primary-content"}`
+            `flex flex-col justify-start items-center md:flex-row gap-2 border rounded-md p-2 ${isActive && "bg-primary text-primary-content"}`
           }
           viewTransition>
           <BiCategoryAlt size={20} />
@@ -68,7 +65,7 @@ export default function AdminLayout() {
           to={"admin/questions"}
           role="tab"
           className={({ isActive }) =>
-            `flex flex-col justify-center items-center md:flex-row gap-2 border rounded-md p-2 ${isActive && "bg-primary text-primary-content"}`
+            `flex flex-col justify-start items-center md:flex-row gap-2 border rounded-md p-2 ${isActive && "bg-primary text-primary-content"}`
           }
           viewTransition>
           <FaQuestion size={20} />
@@ -78,7 +75,7 @@ export default function AdminLayout() {
           to={"admin/orders"}
           role="tab"
           className={({ isActive }) =>
-            `flex flex-col justify-center items-center md:flex-row gap-2 border rounded-md p-2 ${isActive && "bg-primary text-primary-content"}`
+            `flex flex-col justify-start items-center md:flex-row gap-2 border rounded-md p-2 ${isActive && "bg-primary text-primary-content"}`
           }
           viewTransition>
           <HiMiniShoppingBag size={20} />
@@ -88,7 +85,7 @@ export default function AdminLayout() {
           to={"admin/products"}
           role="tab"
           className={({ isActive }) =>
-            `flex flex-col justify-center items-center md:flex-row gap-2 border rounded-md p-2 ${isActive && "bg-primary text-primary-content"}`
+            `flex flex-col justify-start items-center md:flex-row gap-2 border rounded-md p-2 ${isActive && "bg-primary text-primary-content"}`
           }
           viewTransition>
           <AiFillProduct size={20} />
@@ -98,46 +95,25 @@ export default function AdminLayout() {
           to={"admin/users"}
           role="tab"
           className={({ isActive }) =>
-            `flex flex-col justify-center items-center md:flex-row gap-2 border rounded-md p-2 ${isActive && "bg-primary text-primary-content"}`
+            `flex flex-col justify-start items-center md:flex-row gap-2 border rounded-md p-2 ${isActive && "bg-primary text-primary-content"}`
           }
           viewTransition>
           <PiUsersThreeFill size={20} />
           <span className="text-xs md:text-lg">Usuarios</span>
         </NavLink>
+        <NavLink
+          to={"admin/webhooks"}
+          role="tab"
+          className={({ isActive }) =>
+            `flex flex-col justify-start items-center md:flex-row gap-2 border rounded-md p-2 ${isActive && "bg-primary text-primary-content"}`
+          }
+          viewTransition>
+          <RiWebhookFill size={20} />
+          <span className="text-xs md:text-lg">Webhooks</span>
+        </NavLink>
       </div>
       <div className="min-h-screen px-3 w-full">
         <Outlet />
-      </div>
-    </div>
-  );
-}
-
-export function AdminLayoutGrid() {
-  return (
-    // Main container with grid layout for md screens
-    <div className="md:grid md:grid-cols-[272px_1fr] px-2 md:px-0">
-      {/* Sidebar for md screens and up */}
-      <div className="hidden md:block bg-primary/50 min-h-screen">
-        <nav role="tablist" className="flex flex-col sticky top-20">
-          <NavLink to={"/admin/post"} role="tab" className={({ isActive }) => `tab ${isActive && "tab-active"}`} preventScrollReset={true} viewTransition>
-            Posts
-          </NavLink>
-          {/* ... other NavLinks ... */}
-        </nav>
-      </div>
-
-      {/* Main content area */}
-      <div className="flex flex-col">
-        {/* Mobile tabs */}
-        <div role="tablist" className="md:hidden tabs tabs-bordered bg-secondary/50 overflow-x-auto">
-          <NavLink to={"/admin/post"} role="tab" className={({ isActive }) => `tab ${isActive && "tab-active"}`} preventScrollReset={true} viewTransition>
-            Posts
-          </NavLink>
-        </div>
-        {/* Content area */}
-        <main className="p-4">
-          <Outlet />
-        </main>
       </div>
     </div>
   );
