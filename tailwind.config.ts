d182d0857ce198e0f97011afefa4d03a13d867be
Moonflower-labs <134706556+Moonflower-labs@users.noreply.github.com>
2024-11-25 23:43:59 +0000

    removed unused script

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,11 +2,20 @@ import daisyui from "daisyui";
 import type { Config } from "tailwindcss";
 
 export default {
-  content: ["./app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}", "node_modules/preline/dist/*.js"],
+  content: ["./app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       fontFamily: {
-        sans: ['"Inter"', "ui-sans-serif", "system-ui", "sans-serif", '"Apple Color Emoji"', '"Segoe UI Emoji"', '"Segoe UI Symbol"', '"Noto Color Emoji"']
+        sans: [
+          '"Inter"',
+          "ui-sans-serif",
+          "system-ui",
+          "sans-serif",
+          '"Apple Color Emoji"',
+          '"Segoe UI Emoji"',
+          '"Segoe UI Symbol"',
+          '"Noto Color Emoji"'
+        ]
       }
     }
   },
