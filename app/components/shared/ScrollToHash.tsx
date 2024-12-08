6a0e006b28e171d3d1d4609ca0ddd2c38e0da3dd
Moonflower-labs <134706556+Moonflower-labs@users.noreply.github.com>
2024-12-08 21:18:48 +0000

    simplified scrolltohash

--- app/components/shared/ScrollToHash.tsx
+++ app/components/shared/ScrollToHash.tsx
@@ -4,23 +4,17 @@ import { useLocation } from 'react-router';
 function ScrollToHash() {
     const location = useLocation();
     const offset = 190; // Adjust based on your navbar height
-    let timeoutId: NodeJS.Timeout | undefined;
     useEffect(() => {
         if (location.hash) {
-            // Wait for view transition to complete and then scroll
-            timeoutId = setTimeout(() => {
-                const id = location.hash.substring(1)
-                const element = document.getElementById(id);
-                if (element) {
-                    const elementPosition = element.getBoundingClientRect().top + window.scrollY - offset;
-                    window.scrollTo({
-                        top: elementPosition,
-                        behavior: 'smooth',
-                    });
-                }
-            }, 100);
+            const id = location.hash.substring(1)
+            const element = document.getElementById(id);
+            if (element) {
+                const elementPosition = element.getBoundingClientRect().top + window.scrollY - offset;
+                window.scrollTo({
+                    top: elementPosition,
+                });
+            }
         }
-        return () => clearTimeout(timeoutId);
     }, [location]);
 
     return null;
