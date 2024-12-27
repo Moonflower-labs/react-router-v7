8c95262a36cef37bbdda7dbabc3a31a5e0b62cb5
Moonflower-labs <134706556+Moonflower-labs@users.noreply.github.com>
2024-12-27 11:19:24 +0000

    refactor search component

--- app/routes/help.tsx
+++ app/routes/help.tsx
@@ -1,28 +1,51 @@
-import { useState } from "react";
+import { useActionState, useTransition } from "react";
 import { Link } from "react-router";
 import { FadeInComponent } from "~/components/framer-motion/FadeInComponent";
+import type { Route } from "./+types/help";
 
-export default function HelpPage() {
-  const [searchTerm, setSearchTerm] = useState("");
+async function search(_: any, formData: FormData) {
+  let searchTerm = formData.get("search") as string
+  console.log(searchTerm)
+  await new Promise((res) => setTimeout(res, 250))
+  if (!searchTerm) return questions;
 
-  const filteredQuestions = searchTerm ? questions.filter(q => q.question.toLowerCase().includes(searchTerm.toLowerCase())) : questions;
+  return questions.filter(q =>
+    q.question.toLowerCase().includes(searchTerm.toLowerCase().trim())
+  );
+}
+
+
+export default function HelpPage({ }: Route.ComponentProps) {
+  let [, startTransition] = useTransition()
+  let [data, action, pending] = useActionState(search, questions)
+
 
   return (
     <div className="mx-2 mb-4">
       <h1 className="text-4xl text-center pt-4 text-primary mb-8 font-semibold">Página de ayuda</h1>
       <p className="mb-4 mx-auto text-center">A continuación están las preguntas y soluciones más frecuentes:</p>
-      <label htmlFor="search" className="input input-bordered input-sm mb-4 flex items-center gap-2 max-w-sm mx-auto">
-        <input type="text" className="grow" id="search" placeholder="Search" onChange={e => setSearchTerm(e.currentTarget.value)} />
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70">
-          <path
-            fillRule="evenodd"
-            d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
-            clipRule="evenodd"
+      <form action={action}>
+        <label htmlFor="search" className="input input-bordered input-sm mb-4 flex items-center gap-2 max-w-sm mx-auto">
+          <input type="text" name="search" className="grow" id="search" placeholder="Search"
+            onChange={(e) => {
+              startTransition(() => {
+                action(new FormData(e.currentTarget.form!, null))
+              })
+            }}
           />
-        </svg>
-      </label>
+          {pending && <span className="loading loading-spinner loading-xs"></span>}
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70">
+            <path
+              fillRule="evenodd"
+              d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </label>
+      </form>
+
       <div className="flex flex-col gap-3 md:w-2/3 mx-auto">
-        {filteredQuestions.map((q, index) => (
+        {data && data.map((q, index) => (
           <FadeInComponent key={q.id}>
             <div className="collapse collapse-arrow shadow-lg p-4">
               <input type="radio" name="my-accordion-2" />
