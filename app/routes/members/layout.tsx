17978c902d18d903c1a6cf441657064921c7aecb
Moonflower-labs <134706556+Moonflower-labs@users.noreply.github.com>
2025-01-19 10:59:41 +0000

    css fixes

--- app/routes/members/layout.tsx
+++ app/routes/members/layout.tsx
@@ -5,8 +5,6 @@ import spiritImg from "~/icons/plan-spirit.svg"
 import type { Route } from "./+types/layout";
 import { requireUserId } from "~/utils/session.server";
 import ScrollToHash from "~/components/shared/ScrollToHash";
-import { AnimatePresence, motion } from "motion/react";
-import { FaLock, FaLockOpen } from "react-icons/fa6";
 
 
 export async function loader({ request }: Route.LoaderArgs) {
@@ -16,8 +14,9 @@ export async function loader({ request }: Route.LoaderArgs) {
 export default function MembersLayout({ }: Route.ComponentProps) {
 
   return (
-    <AnimatePresence>
+    <>
       <div
+        key={'members-nav'}
         role="navigation"
         data-testid="members-layout"
         className="bg-base-100 flex flex-col gap-1 justify-center sticky top-[72px] z-40 md:w-fit rounded-lg bg-transparent mx-auto pb-5">
@@ -28,38 +27,31 @@ export default function MembersLayout({ }: Route.ComponentProps) {
               to={link.to}
               role="tab"
               className={({ isActive }) =>
-                `flex flex-col justify-center items-center gap-1 rounded-lg p-2 pb-1 shadow-md ${isActive ? "bg-primary text-primary-content" : "bg-base-100"} transition-all ease-out duration-200`
+                `flex flex-col justify-center items-center gap-1 border-[0.5px] rounded-lg p-2 shadow ${isActive ? "bg-primary text-primary-content border-secondary" : "bg-base-100"} transition-all ease-out duration-200`
               }
-            // preventScrollReset={true}
-            // viewTransition
+              // preventScrollReset={true}
+              viewTransition
             >
-              {({ isActive, isPending }) => (
-                <>
-                  <div className="flex flex-row justify-center items-center">
-                    <div className="avatar">
-                      <div className="w-6 rounded">
-                        <img src={link.imageSrc} />
-                      </div>
-                    </div>
-                    <span>{link.name}</span>
+              <div className="flex flex-row gap-1 justify-center items-center">
+                <div className="avatar">
+                  <div className="w-6 rounded">
+                    <img src={link.imageSrc} />
                   </div>
-                  {isActive ? <motion.div className="w-full flex flex-end z-50 h-2" layoutId="underline" ><FaLockOpen /></motion.div>
-                    : <FaLock />}
-                  {/* {isActive ? <motion.div className="block w-full z-50 bg-purple-400 h-2 rounded-md" layoutId="underline" />
-                    : <div className="opacity-0 w-full h-2" />} */}
-                </>
-
-              )}
+                </div>
+                <span>{link.name}</span>
+              </div>
             </NavLink>
           )}
         </div>
         <ScrollToHash />
       </div>
-      <Outlet />
-    </AnimatePresence>
+      <div className="mx-2">
+        <Outlet />
+      </div>
+
+    </>
   );
 }
-
 
 
 const LINKS = [
@@ -67,18 +59,15 @@ const LINKS = [
     to: "/members/personality",
     imageSrc: personalityImg,
     name: "Personalidad"
-
   },
   {
     to: "/members/soul",
     imageSrc: soulImg,
     name: "Alma"
-
   },
   {
     to: "/members/spirit",
     imageSrc: spiritImg,
     name: "Espíritu"
-
   },
 ]
