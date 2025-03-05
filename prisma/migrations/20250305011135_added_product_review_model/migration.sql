b6912728abd4591db22cebf3d55c230cc398de77
Moonflower-labs <134706556+Moonflower-labs@users.noreply.github.com>
2025-03-05 01:13:04 +0000

    added model product review

---
+++ prisma/migrations/20250305011135_added_product_review_model/migration.sql
@@ -0,0 +1,15 @@
+-- CreateTable
+CREATE TABLE "ProductReview" (
+    "id" TEXT NOT NULL,
+    "score" INTEGER NOT NULL,
+    "text" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "productId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "ProductReview_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "ProductReview" ADD CONSTRAINT "ProductReview_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE CASCADE ON UPDATE CASCADE;
